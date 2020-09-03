a6fadd434265c7187950e0a72fdde12b34a3f2ab
craigh210 <craighunt210@gmail.com>
2020-09-03 15:19:33 -0400

    Initial commit

---
+++ src/main/java/edu/mc/chunt/springboot_302/SpringBoot302Application.java
@@ -0,0 +1,13 @@
+package edu.mc.chunt.springboot_302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot302Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/mc/chunt/springboot_302/SpringBoot302ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mc.chunt.springboot_302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
